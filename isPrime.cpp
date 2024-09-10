95305a8dffd873c6b9f5fec20681657be130de67
Shabnam <shabnam.hoypuc1@gmail.com>
2024-09-10 14:05:17 +0530

    Condition check

--- isPrime.cpp
+++ isPrime.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
 bool isPrime(int num) {
+    if (num == 0 || num == 1)   return false;
     for (int i=2; i<num; i++) {
         if(num%i == 0) return false;
     }
     return true;
 }
 int main() {
-    int num =10;
+    int num;
+    cout << "Enter the number: " << endl;
+    cin >> num;
     if(isPrime(num))
-        cout << num << "is a prime number" << endl;
+        cout << num << " is a prime number" << endl;
     else 
         cout << num << " is not a prime number" << endl;
 }
