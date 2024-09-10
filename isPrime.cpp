318bdfa5e68b8d2a676c689c1867785c87ca2949
Shabnam <shabnam.hoypuc1@gmail.com>
2024-09-10 13:35:54 +0530

    Check if a number is prime

---
+++ isPrime.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+bool isPrime(int num) {
+    bool res = true;
+    for (int i=2; i<num; i++) {
+        if(num%i == 0) res = false;
+    }
+    return res;
+}
+int main() {
+    int num =2;
+    if(isPrime(num))
+        cout << num << " is a prime number" << endl;
+    else 
+        cout << num << " is not a prime number" << endl;
+}
